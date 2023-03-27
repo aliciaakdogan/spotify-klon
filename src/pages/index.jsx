2c1bd8d72e7dd095c95f7de8692dadb60896913f
Alicia Akdogan <alicia.akdogan@hotmail.com>
2023-03-28 00:23:54 +0200

    styled sidebar

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,14 +1,15 @@
+import Sidebar from "@/components/Sidebar";
 import React from "react";
 
 export default function index() {
   return (
-    <div className="flex h-screen flex-col bg-red-500">
-      <div className="flex flex-1 bg-pink-300">
-        <aside className="w-full max-w-xs bg-amber-200">sidebar</aside>
-        <main className="flex-1 bg-blue-400"></main>
+    <div className="flex h-screen flex-col ">
+      <div className=" flex flex-1 overflow-y-auto ">
+        <Sidebar />
+        <main className="flex-1 ">main</main>
       </div>
 
-      <footer className="h-20 bg-green-300">footer</footer>
+      <footer className="h-20 ">footer</footer>
     </div>
   );
 }
