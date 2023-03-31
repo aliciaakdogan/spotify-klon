83c75e7058280fbc3f80c7b1674e913bf7d8b474
Alicia Akdogan <alicia.akdogan@hotmail.com>
2023-03-31 13:07:51 +0200

    fixed quick bug with player overlar

--- src/components/PlayerOverlay.jsx
+++ src/components/PlayerOverlay.jsx
@@ -5,7 +5,7 @@ export default function PlayerOverlay({
   setPlayerOverlayIsOpen,
   playerOverlayIsOpen,
   track,
-  localPlayer,
+  player,
   isPaused,
   position,
 }) {
@@ -38,12 +38,14 @@ export default function PlayerOverlay({
           <p className="text-text-dimmer">{track.artists[0].name}</p>
         </div>
         <div className="mt-auto">
-          <PlayerControls
-            player={localPlayer}
-            isPaused={isPaused}
-            position={position}
-            track={track}
-          />
+          {player && (
+            <PlayerControls
+              player={player}
+              isPaused={isPaused}
+              position={position}
+              track={track}
+            />
+          )}
         </div>
       </div>
     </div>
